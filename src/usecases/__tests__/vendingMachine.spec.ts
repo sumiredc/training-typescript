fe46f1b797244ec340ac4301229e1a142a5faeb8
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-09-11 13:50:27 +0900

    テストコード作成

--- src/usecases/__tests__/vendingMachine.spec.ts
+++ src/usecases/__tests__/vendingMachine.spec.ts
@@ -1,12 +1,23 @@
 import {VendingMachine} from '../vendingMachine';
 describe('vendingMachine', () => {
   let vm!: VendingMachine;
+
   beforeEach(() => {
     // テスト前にVendingMachineのインスタンスを作成する
     vm = new VendingMachine();
   });
 
-  it('sample', () => {
-    expect(true).toBeTruthy();
+  it('10円を投入', () => {
+    vm.post(10)
+    expect(vm.balance).toBe(10);
+    
   });
+
+  it('払い戻し', () => {
+    vm.post(10)
+    expect(vm.refund()).toBe(10);
+    expect(vm.balance).toBe(0);
+    
+  });
+
 });
