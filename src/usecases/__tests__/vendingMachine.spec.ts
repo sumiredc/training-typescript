f658df0f9b6efb26ea4d5be3b51e85cb167e24da
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-09-11 15:23:05 +0900

    フォーマットの修正

--- src/usecases/__tests__/vendingMachine.spec.ts
+++ src/usecases/__tests__/vendingMachine.spec.ts
@@ -7,20 +7,47 @@ describe('vendingMachine', () => {
     vm = new VendingMachine();
   });
 
-  it('各種お金を投入', () => {
-    vm.post(10)
-    vm.post(50)
-    vm.post(100)
-    vm.post(1000)
-    expect(vm.balance).toBe(1160);
-    
+  it('10円を投入', () => {
+    vm.post(10);
+    expect(vm.balance).toBe(10);
   });
 
-  it('払い戻し', () => {
-    vm.post(10)
+  it('50円を投入', () => {
+    vm.post(50);
+    expect(vm.balance).toBe(50);
+  });
+
+  it('100円を投入', () => {
+    vm.post(100);
+    expect(vm.balance).toBe(100);
+  });
+
+  it('500円を投入', () => {
+    vm.post(500);
+    expect(vm.balance).toBe(500);
+  });
+
+  it('1000円を投入', () => {
+    vm.post(1000);
+    expect(vm.balance).toBe(1000);
+  });
+
+  it('複数回(10回)10円を投入', () => {
+    for (let i = 0; i < 10; i++) {
+      vm.post(10);
+    }
+    expect(vm.balance).toBe(100);
+  });
+
+  it('おつりを出す', () => {
+    vm.post(10);
     expect(vm.refund()).toBe(10);
     expect(vm.balance).toBe(0);
-    
   });
 
+  it('使えないお金を返金する', () => {
+    vm.post(10);
+    expect(vm.refund()).toBe(10);
+    expect(vm.post(5)).toBe(5);
+  });
 });
