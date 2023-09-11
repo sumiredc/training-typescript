c93eeb6fc46e0402040e86cb469a2bb1a3e7f73a
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-09-11 13:49:34 +0900

    テストコード作成

--- src/usecases/vendingMachine.ts
+++ src/usecases/vendingMachine.ts
@@ -1,1 +1,21 @@
-export class VendingMachine {}
+export class VendingMachine {
+    
+    private internalBalance:number=0
+
+    post(coin:number){
+        this.internalBalance+=coin;
+    }
+
+    get balance(){
+        return this.internalBalance
+    }
+
+    refund(){
+        const change:number=this.internalBalance
+        this.internalBalance=0
+        return change
+
+
+    }
+
+}
