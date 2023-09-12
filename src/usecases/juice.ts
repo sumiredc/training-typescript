a0af246aa8681b6ef3f842cc02e1cf85af009ff1
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-09-13 00:26:04 +0900

    ジュースクラスの生成

--- src/usecases/juice.ts
+++ src/usecases/juice.ts
@@ -19,7 +19,25 @@ export class Juice {
     return this.internalPrice;
   }
 
-  get stocks(): number {
+  get stock(): number {
     return this.internalStock;
+  }
+
+  set name(name: string) {
+    this.internalName = name;
+  }
+
+  set price(price: number) {
+    this.internalPrice = price;
+  }
+
+  set stock(stock: number) {
+    this.internalStock = stock;
+  }
+
+  juiceInfo(): string {
+    return (
+      'name:' + this.name + ' price:' + this.price + ' stock:' + this.stock
+    );
   }
 }
