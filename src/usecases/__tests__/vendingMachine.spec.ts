f37976dcd63524eaf19bcaf7a53bbe1bdc8d819b
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-09-12 13:59:15 +0900

    enumでの実装に伴うテストケースの変更

--- src/usecases/__tests__/vendingMachine.spec.ts
+++ src/usecases/__tests__/vendingMachine.spec.ts
@@ -1,4 +1,6 @@
 import {VendingMachine} from '../vendingMachine';
+import {MoneyType} from '../money';
+
 describe('vendingMachine', () => {
   let vm!: VendingMachine;
 
@@ -8,45 +10,45 @@ describe('vendingMachine', () => {
   });
 
   it('10円を投入', () => {
-    vm.post(10);
+    vm.post(MoneyType.TEN);
     expect(vm.balance).toBe(10);
   });
 
   it('50円を投入', () => {
-    vm.post(50);
+    vm.post(MoneyType.FIFTY);
     expect(vm.balance).toBe(50);
   });
 
   it('100円を投入', () => {
-    vm.post(100);
+    vm.post(MoneyType.HUNDRED);
     expect(vm.balance).toBe(100);
   });
 
   it('500円を投入', () => {
-    vm.post(500);
+    vm.post(MoneyType.FIVE_HUNDRED);
     expect(vm.balance).toBe(500);
   });
 
   it('1000円を投入', () => {
-    vm.post(1000);
+    vm.post(MoneyType.THOUSAND);
     expect(vm.balance).toBe(1000);
   });
 
   it('複数回(10回)10円を投入', () => {
     for (let i = 0; i < 10; i++) {
-      vm.post(10);
+      vm.post(MoneyType.TEN);
     }
     expect(vm.balance).toBe(100);
   });
 
   it('おつりを出す', () => {
-    vm.post(10);
+    vm.post(MoneyType.TEN);
     expect(vm.refund()).toBe(10);
     expect(vm.balance).toBe(0);
   });
 
   it('使えないお金を返金する', () => {
-    vm.post(10);
+    vm.post(MoneyType.TEN);
     expect(vm.refund()).toBe(10);
     expect(vm.post(5)).toBe(5);
   });
