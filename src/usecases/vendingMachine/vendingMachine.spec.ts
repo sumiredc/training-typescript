1c64135cdae5d656acd301882d03328b3155500f
rintaro yamada <yamada.rintaro.vkc0@gmail.com>
2023-10-04 17:43:21 +0900

    テストケース　モック化

--- src/usecases/vendingMachine/vendingMachine.spec.ts
+++ src/usecases/vendingMachine/vendingMachine.spec.ts
@@ -1,112 +1,83 @@
 import { VendingMachine } from './vendingMachine';
 import { MoneyType } from '../../domain/objects/moneyType';
-import { JuiceType, JuiceData } from '../../domain/objects/juiceType';
-import { Display } from '../display/display';
-import { Juice } from '../../domain/entities/juice';
+import { JuiceType } from '../../domain/objects/juiceType';
 import { Stock } from '../stock/stock';
+let vm!: VendingMachine;
+let s!: Stock;
 
-describe('vendingMachine', () => {
-  let vm!: VendingMachine;
-  let d!: Display;
+beforeEach(() => {
+  // テスト前にVendingMachineのインスタンスを作成する
+  vm = new VendingMachine();
+  s = new Stock();
+});
 
-  beforeEach(() => {
-    // テスト前にVendingMachineのインスタンスを作成する
-    vm = new VendingMachine();
-    d = new Display();
+describe.each([
+  [MoneyType.ONE, 1],
+  [MoneyType.FIVE, 5],
+  [MoneyType.TEN, 0],
+  [MoneyType.FIFTY, 0],
+  [MoneyType.HUNDRED, 0],
+  [MoneyType.FIVE_HUNDRED, 0],
+  [MoneyType.THOUSAND, 0],
+  [MoneyType.FIVE_THOUSAND, 5000],
+  [MoneyType.TEN_THOUSAND, 10000],
+])('お金を投入', (MoneyType: MoneyType, expected: number) => {
+  it(`post ${MoneyType}`, () => {
+    expect(vm.post(MoneyType)).toBe(expected);
   });
+});
 
-  it('10円を投入', () => {
-    vm.post(MoneyType.TEN);
-    expect(vm.cash.balance).toBe(10);
-  });
+it('おつりを出す', () => {
+  vm.post(MoneyType.FIVE_HUNDRED);
+  expect(vm.refund()).toBe(500);
+});
 
-  it('50円を投入', () => {
-    vm.post(MoneyType.FIFTY);
-    expect(vm.cash.balance).toBe(50);
-  });
+describe('ジュースが購入可能かどうか判断する', () => {
+  const mockedIsNotSoldOout = jest.spyOn(s, 'isNotSoldOut');
+  const mockedIsOrverEqualPrice = jest.spyOn(s, 'isOverEqualPrice');
 
-  it('100円を投入', () => {
-    vm.post(MoneyType.HUNDRED);
-    expect(vm.cash.balance).toBe(100);
-  });
-
-  it('500円を投入', () => {
-    vm.post(MoneyType.FIVE_HUNDRED);
-    expect(vm.cash.balance).toBe(500);
-  });
-
-  it('1000円を投入', () => {
-    vm.post(MoneyType.THOUSAND);
-    expect(vm.cash.balance).toBe(1000);
-  });
-
-  it('複数回(10回)10円を投入', () => {
-    for (let i = 0; i < 10; i++) {
-      vm.post(MoneyType.TEN);
-    }
-    expect(vm.cash.balance).toBe(100);
-  });
-
-  it('おつりを出す', () => {
-    vm.post(MoneyType.FIVE_HUNDRED);
-    expect(vm.cash.balance).toBe(500);
-    expect(vm.refund()).toBe(500);
-  });
-
-  it('使えないお金(1円)を返金する', () => {
-    expect(vm.post(MoneyType.FIVE)).toBe(5);
-    expect(vm.cash.balance).toBe(0);
-  });
-
-  it('使えないお金(5円)を返金する', () => {
-    expect(vm.post(MoneyType.ONE)).toBe(1);
-    expect(vm.cash.balance).toBe(0);
-  });
-
-  it('使えないお金(5000円)を返金する', () => {
-    expect(vm.post(MoneyType.FIVE_THOUSAND)).toBe(5000);
-    expect(vm.cash.balance).toBe(0);
-  });
-
-  it('使えないお金(1万円)を返金する', () => {
-    expect(vm.post(MoneyType.TEN_THOUSAND)).toBe(10000);
-    expect(vm.cash.balance).toBe(0);
-  });
-
-  it('購入可能なドリンクのリストを取得する', () => {
-    vm.post(MoneyType.HUNDRED);
-    vm.post(MoneyType.FIFTY);
-    expect(vm.cash.balance).toBe(150);
-    expect(vm.acquireBuyableList(vm.stock.stocks)).toEqual(
-      new Map([
-        [
-          JuiceType.COKE,
-          {
-            juice: vm.stock.getStockJuice(JuiceType.COKE),
-            quantity: vm.stock.getStockQuantity(JuiceType.COKE),
-          },
-        ],
-        [
-          JuiceType.WATER,
-          {
-            juice: vm.stock.getStockJuice(JuiceType.WATER),
-            quantity: vm.stock.getStockQuantity(JuiceType.WATER),
-          },
-        ],
-      ])
+  it('testが成功する場合', () => {
+    mockedIsNotSoldOout.mockResolvedValue(true);
+    mockedIsOrverEqualPrice.mockResolvedValue(true);
+      }
     );
+    expect(mockedIsNotSoldOout).toBe(true);
   });
+});
 
-  it('コーラを購入する', () => {
-    vm.post(MoneyType.FIVE_HUNDRED);
-    expect(vm.cash.balance).toBe(500);
-    expect(vm.buying(JuiceType.COKE)).toBe(380);
-    expect(vm.cash.balance).toBe(0);
-  });
+it('購入可能なドリンクのリストを取得する', () => {
+  vm.post(MoneyType.HUNDRED);
+  vm.post(MoneyType.FIFTY);
+  expect(vm.cash.balance).toBe(150);
+  expect(vm.acquireBuyableList(vm.stock.stocks)).toEqual(
+    new Map([
+      [
+        JuiceType.COKE,
+        {
+          juice: vm.stock.getStockJuice(JuiceType.COKE),
+          quantity: vm.stock.getStockQuantity(JuiceType.COKE),
+        },
+      ],
+      [
+        JuiceType.WATER,
+        {
+          juice: vm.stock.getStockJuice(JuiceType.WATER),
+          quantity: vm.stock.getStockQuantity(JuiceType.WATER),
+        },
+      ],
+    ])
+  );
+});
 
-  it('売上を確認する', () => {
-    vm.post(MoneyType.FIVE_HUNDRED);
-    vm.buying(JuiceType.COKE);
-    expect(vm.cash.earning).toBe(120);
-  });
+it('コーラを購入する', () => {
+  vm.post(MoneyType.FIVE_HUNDRED);
+  expect(vm.cash.balance).toBe(500);
+  expect(vm.buying(JuiceType.COKE)).toBe(true);
+  expect(vm.cash.balance).toBe(380);
+});
+
+it('売上を確認する', () => {
+  vm.post(MoneyType.FIVE_HUNDRED);
+  vm.buying(JuiceType.COKE);
+  expect(vm.cash.earning).toBe(120);
 });
